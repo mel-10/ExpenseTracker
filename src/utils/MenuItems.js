ec74d84237983b436ac663070de0f747f1b606c6
mel-10 <mkaramkar@gmail.com>
2024-01-16 15:35:56 +0530

    frontend

---
+++ src/utils/MenuItems.js
@@ -0,0 +1,26 @@
+import  {transactions,dashboard, expenses,trend } from '../utils/Icons'
+export const menuItems =[
+    {
+        id:1,
+        title:"Dashboard",
+        icon:dashboard,
+        link:'/dashboard'
+    },{
+        id: 2,
+        title: "View Transactions",
+        icon: transactions,
+        link: "/dashboard",
+    },
+    {
+        id: 3,
+        title: "Incomes",
+        icon: trend,
+        link: "/dashboard",
+    },
+    {
+        id: 4,
+        title: "Expenses",
+        icon: expenses,
+        link: "/dashboard",
+    },
+]
